f9d96ebdd759303bfd201bfa148fafa87fb76d98
vishnoiprem <pvishnoi@MKHOMBP14018.local>
2024-07-20 21:17:00 +0700

    add'

--- code/agoda/num_to_code.py
+++ code/agoda/num_to_code.py
@@ -1,5 +1,5 @@
 
-Convert the given Number to a Code and vice-versa
+# Convert the given Number to a Code and vice-versa
 
 # A number can only be in the range of 1 to 190. A number can be transformed to code "xy" such that x is in range [A, S] and y is in range [0,9] and is in increasing order.
 # For example:
@@ -17,16 +17,28 @@ Convert the given Number to a Code and vice-versa
 # f("J9") = 100
 
 
+def number_to_code(number):
+    if number < 1 or number > 190:
+        raise ValueError("Number out of range")
 
-def num_to_code(num):
+    letters = "ABCDEFGHIJKLMNOPQRS"
+    quotient = (number - 1) // 10
+    remainder = (number - 1) % 10
 
-    dict_num = 'abcdefghijklmnopqrst'
-
-    numerator = num//10
-    denominator = num%10
-
-    dist_umn=denominator+
-
-
+    return f"{letters[quotient]}{remainder}"
 
 
+def code_to_number(code):
+    if len(code) != 2 or not ('A' <= code[0] <= 'S') or not ('0' <= code[1] <= '9'):
+        raise ValueError("Invalid code format")
+
+    letters = "ABCDEFGHIJKLMNOPQRS"
+    letter_value = letters.index(code[0])
+    digit_value = int(code[1])
+
+    return letter_value * 10 + digit_value + 1
+
+
+# Testing the functions
+print(number_to_code(100))  # Output should be "J9"
+print(code_to_number("J9"))  # Output should be 100
