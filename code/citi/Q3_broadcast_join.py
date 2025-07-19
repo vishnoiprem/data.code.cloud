2cafe9c0a403633dad8a7c448c79987b7692e41a
Prem Vishnoi <vishnoiprem@Prems-iMac.local>
2025-07-19 15:48:56 +0700

    cpde

--- code/citi/Q3_broadcast_join.py
+++ code/citi/Q3_broadcast_join.py
@@ -1,25 +1,30 @@
 # Q3: Join a large transactions table with a small customer table using broadcast join (PySpark + Spark SQL)
 
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import broadcast
+from pyspark.sql.functions import broadcast, sum, col
 
 spark = SparkSession.builder.appName("Q3_BroadcastJoin").getOrCreate()
+spark.conf.set("spark.sql.autoBroadcastJoinThreshold", "100MB")
+spark.conf.set("spark.sql.join.preferSortMergeJoin", "false")
+
+
+
+
 
 # Load data
 df_transactions = spark.read.option("header", True).csv("data/transactions.csv")
 df_customers = spark.read.option("header", True).csv("data/customers.csv")
-
 # PySpark Way
 df_joined = df_transactions.join(broadcast(df_customers), "customer_id", "left")
-df_joined.write.mode("overwrite").parquet("output/q3_result/")
+df_joined.write.mode("overwrite").csv("output/q3_result/")
 
 # Spark SQL Way
 df_transactions.createOrReplaceTempView("transactions")
 df_customers.createOrReplaceTempView("customers")
 
 df_sql = spark.sql("""
-    SELECT *
-    FROM transactions LEFT JOIN customers
-    ON transactions.customer_id = customers.customer_id
+    SELECT t.*, c.customer_id as customer_ids,c.name,c.email
+    FROM transactions as t LEFT JOIN customers as c
+    ON t.customer_id = c.customer_id
 """)
-df_sql.write.mode("overwrite").parquet("output/q3_result_sql/")
+df_sql.write.mode("overwrite").csv("output/q3_result_sql/")
