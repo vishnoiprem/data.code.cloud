37af38b21a269a7dd50f147327bfa685dc61993c
Prem Vishnoi <vishnoiprem@Prems-iMac.local>
2025-07-19 15:21:14 +0700

    cpde

--- code/citi/Q2_clean_transaction_type.py
+++ code/citi/Q2_clean_transaction_type.py
@@ -10,14 +10,14 @@ df = spark.read.option("header", True).csv("data/transactions.csv")
 
 # PySpark Way
 df_cleaned = df.withColumn("transaction_type", when(col("transaction_type").isNull(), "UNKNOWN").otherwise(col("transaction_type")))
-df_cleaned.write.mode("overwrite").parquet("output/q2_result/")
+df_cleaned.write.mode("overwrite").csv("output/q2_result/")
 
 # Spark SQL Way
 df.createOrReplaceTempView("transactions")
 
 df_sql = spark.sql("""
     SELECT *,
-           CASE WHEN transaction_type IS NULL THEN 'UNKNOWN' ELSE transaction_type END AS transaction_type_fixed
+           CASE WHEN coalesce( transaction_type,"")="" THEN 'UNKNOWN' ELSE transaction_type END AS transaction_type_fixed
     FROM transactions
 """)
-df_sql.write.mode("overwrite").parquet("output/q2_result_sql/")
+df_sql.write.mode("overwrite").csv("output/q2_result_sql/")
